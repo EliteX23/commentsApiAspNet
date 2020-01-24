4684c7ca50353d98995d444cf7129a8cec20a284
Alex <elitex25@gmail.com>
2020-01-24 14:19:18 +0300

    Реализация репозитория для токенов

---
+++ commentsApiAspNet/Infrastructure/Repositories/SecurityRepository.cs
@@ -0,0 +1,41 @@
+﻿using commentsApiAspNet.Domain.Core;
+using commentsApiAspNet.Domain.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace commentsApiAspNet.Infrastructure.Repositories
+{
+    public class SecurityRepository : ITokenRepository
+    {
+        private IMongoCollection<Token> _collection;
+     
+        public SecurityRepository(IMongoDatabase database)
+        {                  
+            _collection = database.GetCollection<Token>("Api");          
+        }
+
+        public async Task<DeleteResult> Delete(string id)
+        {
+            return await _collection.DeleteOneAsync(apiKey => apiKey.Key == id);
+        }
+
+        public async Task<Token> Get(string id)
+        {
+            return await _collection.FindAsync(apiKey => apiKey.Key == id).Result.FirstOrDefaultAsync();
+        }
+
+        public async Task Save(Token item)
+        {
+            await _collection.InsertOneAsync(item);
+        }
+
+        public async Task<ReplaceOneResult> Update(string id, Token item)
+        {
+            return await _collection.ReplaceOneAsync(apiKey => apiKey.Key == id, item);
+        }
+    }
+}
