c1054c621b49f9803f17898f01bda96c5e8dae34
Alex <elitex25@gmail.com>
2020-01-24 14:16:55 +0300

    интерфейс Сервиса авторизации

---
+++ commentsApiAspNet/Domain/Interfaces/IToken.cs
@@ -0,0 +1,21 @@
+﻿using commentsApiAspNet.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace commentsApiAspNet.Domain.Interfaces
+{
+    //Интерфейс сервиса авторизации
+    public interface IToken
+    {
+        Task<bool> IsValid(string apikey, string method);
+
+        //Методы только для теста
+        Task<Token> GetApiKey(string id);
+        Task Save(Token key);
+        Task Update(string id, Token item);
+        Task Delete(string id);
+    }
+}
---
+++ commentsApiAspNet/Domain/Interfaces/ITokenRepository.cs
@@ -0,0 +1,19 @@
+﻿using commentsApiAspNet.Domain.Core;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace commentsApiAspNet.Domain.Interfaces
+{
+    public interface ITokenRepository
+    {
+        Task<Token> Get(string id);
+        //метод предназначен исключительно для заполнения БД.
+        Task Save(Token item);
+        Task<ReplaceOneResult> Update(string id, Token item);
+        Task<DeleteResult> Delete(string id);
+    }
+}
