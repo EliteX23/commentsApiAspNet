2db3e59838ca5e84e9b376ee33f08d46f82380ee
Alex <elitex25@gmail.com>
2020-01-24 14:14:43 +0300

    Добавление модели токена

---
+++ commentsApiAspNet/Domain/Core/Token.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace commentsApiAspNet.Domain.Core
+{
+    public class Token
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        //Практически баг. Поле ключ не уникальное. Следовательно может иметь разные значения, но так как имеется ввиду, что уникальность контролируется человеком
+        public string Key { get; set; }
+        //для простоты разрешенные типы будут храниться через запятую
+        public string GrantedRequest { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
