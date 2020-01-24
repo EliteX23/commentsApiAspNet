612e5761b96080141edd675f77ad1f34e8ee50a2
Alex <elitex25@gmail.com>
2020-01-24 13:00:59 +0300

    Добавлена модель комментария

---
+++ commentsApiAspNet/Domain/Core/Comment.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace commentsApiAspNet.Domain.Core
+{
+    public class Comment
+    {
+        [Key]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string Author { get; set; }
+        public string Topic { get; set; }
+
+    }
+}
