4df4524d37159326e97534ed8f1a0be5ae11b5ed
Alex <elitex25@gmail.com>
2020-01-24 13:03:25 +0300

    Интерфейс сервиса коментариев

---
+++ commentsApiAspNet/Domain/Interfaces/IComment.cs
@@ -0,0 +1,18 @@
+﻿using commentsApiAspNet.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace commentsApiAspNet.Domain.Interfaces
+{
+    public interface IComment
+    {
+        Task<IEnumerable<Comment>> GetList();
+        Task<Comment> GetComment(string id);
+        Task Save(Comment comment);
+        Task Update(string id, Comment item);
+        Task Delete(string id);
+    }
+}
