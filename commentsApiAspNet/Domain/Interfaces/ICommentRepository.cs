62a0ac0f55d367333502e48d599cef6bceee6bd9
Alex <elitex25@gmail.com>
2020-01-24 13:02:30 +0300

    Интерфейс репозитория коментариев

---
+++ commentsApiAspNet/Domain/Interfaces/ICommentRepository.cs
@@ -0,0 +1,20 @@
+﻿using commentsApiAspNet.Domain.Core;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace commentsApiAspNet.Domain.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetCommentList();
+        Task<Comment> GetComment(string id);
+        Task Create(Comment item);
+        Task<ReplaceOneResult> Update(string id, Comment item);
+        Task<DeleteResult> Delete(string id);
+
+    }
+}
